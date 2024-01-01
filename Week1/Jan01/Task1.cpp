50b3b095d85ed7aba942d923dbca7728d4dfb0df
Sourav <srvmandal24@gmail.com>
2024-01-01 13:38:47 +0530

    Added description to both codes

--- Week1/Jan01/Task1.cpp
+++ Week1/Jan01/Task1.cpp
@@ -1,4 +1,14 @@
-// Given an array Arr of size N, print second largest distinct element from an array.
+/* Given an array Arr of size N, print second largest distinct element from an array.
+Link : https://practice.geeksforgeeks.org/problems/second-largest3735/1  (GFG-Second Largest)
+*/
+
+// Submitted on 01/01/2024
+
+/*  Time Complexity : O(n)
+    Space Complexity : O(1) */
+
+#include <bits/stdc++.h>
+using namespace std;
 
 class Solution{
 public:	
--- Week1/Jan01/Task2.cpp
+++ Week1/Jan01/Task2.cpp
@@ -1,9 +1,21 @@
-// Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+/* Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+Link : https://leetcode.com/problems/missing-number/  (LC-268 Missing Number)
+*/
+
+// Submitted on 01/01/2024
+
+/*  Sort given vector then compare index to the element
+    Time Complexity : O(nlogn)
+    Space Complexity : O(1) */
+
+#include <bits/stdc++.h>
+using namespace std;
 
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int n = nums.size(), i;
+
         sort(nums.begin(), nums.end());
         
         for(i=0; i<n; i++)
